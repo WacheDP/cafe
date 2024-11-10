6703bada2760a463f01c95fca69fdcdf1f82169b
Daniel Parra <wacheparra21@gmail.com>
2024-11-10 18:59:20 -0400

    Plantilla Homepage

---
+++ php/head.php
@@ -0,0 +1,12 @@
+<section id="cabecera">
+
+    <h1>CAFÉ DON BELEN</h1>
+
+    <nav id="menu">
+        <a href="#">Inicio</a>
+        <a href="#">Registrarse</a>
+        <a href="#">Iniciar Sesión</a>
+        <a href="./home.php#contactanos">Contactanos</a>
+    </nav>
+
+</section>
