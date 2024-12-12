6e3fd5c9882cd3e92457827204bffd54a0f8ee1d
Daniel Parra <wacheparra21@gmail.com>
2024-12-11 21:20:58 -0400

    Ultimo Avance

--- home.php
+++ home.php
@@ -5,9 +5,169 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Pagina Principal</title>
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <title>Página de Inicio</title>
 </head>
 
-<body></body>
+<body>
+    <!-- Encabezado -->
+    <header>
+
+        <div class="header">
+            <img src="./recursos/Logo.png" class="logo" alt="Logo">
+
+            <div class="inicio">
+                <h5 class="card-title" style="font-size: 14px;">Inicio De Sesion</h5> <!-- Texto reducido -->
+                <form action="./php/procesar_login.php" method="post">
+                    <div class="form-group row caja-input">
+                        <label for="ID" class="col-sm-4 col-form-label">Usuario</label> <!-- Texto reducido -->
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="ID" name="ID" placeholder="user21" style="font-size: 12px; height: 25px;"> <!-- Ajusta la altura del input -->
+                            <small id="help" class="form-text text-muted" style="font-size: 10px;">* para la identificación puedes utilizar tu nombre de usuario o tu correo electrónico *</small> <!-- Texto reducido -->
+                        </div>
+                    </div>
+
+                    <div class="form-group row caja-input">
+                        <label for="password" class="col-sm-4 col-form-label">Contraseña</label> <!-- Texto reducido -->
+                        <div class="col-sm-8">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="****" style="font-size: 12px; height: 25px;"> <!-- Ajusta la altura del input -->
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-sm-12">
+                            <input type="submit" class="btn btn-outline-success" name="btn" value="Entrar" style="font-size: 12px; height: 30px;"> <!-- Ajusta la altura del bot�n -->
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="container">
+            <h1>Bienvenido a Cafe Don Belen</h1>
+            <nav>
+                <ul>
+                    <li><a href="#carouselExampleDark">Sobre Nosotros</a></li>
+                    <li><a href="#socios">Socios</a></li>
+                    <li><a href="#contact-form">Contactanos</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div id="carouselExampleDark" class="carousel carousel-dark slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <img src="./recursos/img/img1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>en un pequeño pueblo del municipio lobatera, llamado borota</h5>
+                    <p>se encuentra la finca de café "Don Belén". Esta finca no es solo un lugar de producción, sino un homenaje a Don Belén, el abuelo de la matriarca de la familia, quien dedicó su vida a trabajar esta tierra con amor y dedicación..</p>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="./recursos/img/img2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>La historia de la finca "Don Belén"</h5>
+                    <p>se remonta a la cuarentena de 2020, cuando la pandemia de COVID-19 azotó a Venezuela y al mundo entero. Durante este tiempo de incertidumbre, una familia compuesta por un padre, una madre y sus dos hijos decidió aprovechar el tiempo en aislamiento para restaurar y revitalizar la finca que alguna vez fue un basurero, pero que en sus días de gloria fue el orgullo de Don Belén.
+
+                        Con determinación y un profundo sentido de honor hacia su ancestro, la familia se mudó a la finca. Día tras día, trabajaron incansablemente para recuperar el terreno, limpiando y preparando la tierra para el cultivo de café. A través de arduo trabajo y sacrificio, lograron transformar el lugar en un próspero campo de café, reflejando el espíritu laborioso de Don Belén..</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./recursos/img/img3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>El renacimiento de la finca</h5>
+                    <p>simboliza el profundo respeto y gratitud hacia las generaciones pasadas. En honor a Don Bel�n, la finca y el caf� que hoy en d�a distribuyen llevan su nombre, asegurando que su legado perdure y siga inspirando a futuras generaciones.
+
+                        La finca de caf� "Don Bel�n" es ahora un testimonio de la evoluci�n y el esfuerzo conjunto de una familia que, en medio de una pandemia global, encontr� fuerza en sus ra�ces y cre� algo hermoso y significativo. Cada grano de caf� cultivado y cada taza servida cuenta una historia de dedicaci�n, honor y evoluci�n...</p>
+                </div>
+            </div>
+        </div>
+        <button id="boton" class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button id="boton" class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <section id="socios" class="socios">
+        <div class="container">
+            <h2>Nuestros Fundadores</h2>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="recursos/img/so.jpg" alt="Socio 1" class="card-img-top">
+                        <div class="card-body">
+                            <h3 class="card-title">David Parra</h3>
+                            <p class="card-text">Fundador con el 40% de la Acciones.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="recursos/img/so.jpg" alt="Socio 2" class="card-img-top">
+                        <div class="card-body">
+                            <h3 class="card-title">Daniel Parra</h3>
+                            <p class="card-text">Fundador con el 40% de la Acciones.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="recursos/img/so.jpg" alt="Socio 3" class="card-img-top">
+                        <div class="card-body">
+                            <h3 class="card-title">Jose Morales</h3>
+                            <p class="card-text">CO-Fundador con el 10% de la Acciones.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="recursos/img/so.jpg" alt="Socio 4" class="card-img-top">
+                        <div class="card-body">
+                            <h3 class="card-title">Enyi Safir</h3>
+                            <p class="card-text">CO-Fundador con el 10% de la Acciones.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section id="contact-form" class="contact-form">
+            <div class="form-container">
+                <h2>Contactanos</h2>
+                <form action="#" method="POST">
+                    <label for="name">Nombre:</label>
+                    <input type="text" id="name" name="name" required>
+
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+
+                    <label for="phone">Telefono:</label>
+                    <input type="tel" id="phone" name="phone">
+
+                    <label for="message">Mensaje:</label>
+                    <textarea id="message" name="message" rows="4" required></textarea>
+
+                    <button type="submit" class="submit-button">Enviar</button>
+                </form>
+            </div>
+        </section>
+    </section>
+    <footer>
+        <p>&copy; 2024 Don Belen. Todos los derechos reservados.</p>
+    </footer>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 
 </html>
--- sistema/inicio.php
+++ sistema/inicio.php
@@ -6,23 +6,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Logo -->
-
-    <!-- Estilos -->
+    <link rel="stylesheet" href="../css/inicio.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
     <title>Café Don Belén</title>
 </head>
 
 <body>
     <?php require "./navbar.php"; ?>
+    <main>
 
-    <h1><?php echo 'BIENVENIDO ' . $_SESSION['usuario']; ?></h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque, sed recusandae numquam qui facilis officiis. Modi expedita maxime voluptatibus dicta architecto numquam recusandae laudantium, ab, nostrum voluptas vero provident eaque.</p>
+        <h1><?php echo 'BIENVENIDO ' . $_SESSION['usuario']; ?></h1>
+
+    </main>
 </body>
 
-<!-- Javascripts -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </html>
